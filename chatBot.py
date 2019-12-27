1605564e1d93657be14fff2d3e7823c7f22363dd
roshan172 <pandeyroshan556@gmail.com>
2019-12-27 18:28:52 +0530

    Typing effect Added

--- chatBot.py
+++ chatBot.py
@@ -1,53 +1,50 @@
 import sqlite3
 import random
+import sys
+from time import sleep
 
-def get_database_cursor():
-    con=sqlite3.Connection('ChatbotDB')
-    cur=con.cursor()
-    return cur
 
+def interactive_print(input_string):
+    for char in input_string:
+        sleep(0.1)
+        sys.stdout.write(char)
+        sys.stdout.flush()
+    print()
+
+def setup_database():
+    con = sqlite3.Connection('ChatbotDB')
+    cur = con.cursor()
+    cur.execute('create table if not exists user_credentials(username varchar(30),password varchar(30))')
 
 
 def start_bot():
-    statement = 'Hey ! Are you a registered user or a new user'
 
 
     def new_user_setup():
-        statement = 'Hello User, What should I call you from next time?'
-        print('Alexa: ',statement)
+        statement = 'Alexa: Hello User, What should I call you from next time?'
+        interactive_print(statement)
         command = input('You: ')
         if len(command.split(" "))<=2:
-            statement = 'I will simply call you, '+str(command.split(" ")[0])+'. Is this ok for you ?'
-            print(statement)
-            bool_command = input('You: ')
-            if bool_command.find('yes') or bool_command.find('ok') or  bool_command.find('good'):
-                print('Alexa: OK, '+str(command.split(" ")[0]))
-                passcode = str(random.randint(1000,9999))
-                print('Alexa: Here is your auto generated PIN',passcode,' would you like to keep this?')
-                passcode_bool_command = input('You: ')
-                if passcode_bool_command.find('yes') or passcode_bool_command.find('ok') or  passcode_bool_command.find('good'):
-                    pass
-                else:
-                    print('Alexa: What PIN I should keep for you ?')
-                    command = input('You: ')
-                    con = sqlite3.Connection('ChatbotDB')
-                    cur = con.cursor()
-                    cur.execute('create table if not exists user_credentials(username varchar(30),password varchar(30))')
-                    con.commit()
-            else:
-                statement = 'What I should call you then ?'
-                command = input('You: ')
-                print('Alexa: OK',command)
-
+            statement = 'Alexa: I will simply call you, '+str(command.split(" ")[0])+'.'
+            interactive_print(statement)
+            passcode_command = str(random.randint(1000,9999))
+            statement = 'Alexa: Here is your auto generated PIN '+str(passcode_command)+' would you like to keep this or set a new one?'
+            interactive_print(statement)
+            passcode_bool_command = input('You: ')
+            if 'new' in passcode_bool_command.split(" "):
+                interactive_print('Alexa: What should be your password then ?')
+                passcode_command = input('You: ')
+            interactive_print('Alexa: Here is your login credential, \nUsername: '+str(command)+'\nPassword: '+str(passcode_command))
+            interactive_print('Alexa: Now, I\'m redirecting you to login window and within 3 second this window will be gone')
 
     def login():
         pass
 
 
     def register_login():
-        statement = 'Hey ! Are you a registered user or a new user'
+        statement = 'Alexa: Hey ! Are you a registered user or a new user'
         while True:
-            print('Alexa: ',statement)
+            interactive_print(statement)
             command = input('You: ')
             if str(command).find('new') or str(command).find('first') or str(command).find('before'):
                 new_user_setup()
@@ -60,4 +57,5 @@ def start_bot():
 
 
 if __name__=="__main__":
+    setup_database()
     start_bot()
---
+++ test.py
@@ -0,0 +1,3 @@
+myStr = 'Hello this is Roshan'
+if 'in' in myStr:
+    print('Hello')
