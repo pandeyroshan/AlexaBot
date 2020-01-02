af3734870f2042890f60f4c468b45abd71110a7e
roshan172 <pandeyroshan556@gmail.com>
2020-01-02 12:34:42 +0530

    send_mail implemented

--- start.py
+++ start.py
@@ -2,7 +2,13 @@ from utils import interactive_print
 import os
 from utils import (
     tell_joke,
-    clear_screen
+    clear_screen,
+    get_privious_date,
+    get_next_date,
+    get_date,
+    who_is,
+    word_meaning,
+    send_mail
 )
 
 
@@ -18,14 +24,31 @@ def match_command_with_function(command):
     if 'hi' in command or 'Hello' in command or 'hello' in command or 'Hi' in command:
         interactive_print('Alexa: Hi, I can do various things for you, just type \'what you can do\' to know more.')
     
+    if 'date' in command and 'yesterday' in command:
+        get_privious_date()
+    
+    elif 'date' in command and 'tommorow' in command:
+        get_next_date()
+    
+    elif 'date' in command:
+        get_date()
     
     if 'what you can do' in command:
         interactive_print('I can do following task')
-        print('1. Tell a joke\n2. Dates\n3. Famous Personality Name')
+        print('1. Tell a joke\n2. Dates\n3. Word meaning')
     
     
     if 'joke' in command:
         tell_joke()
+    
+    if 'who is' in command:
+        who_is()
+    
+    if 'mean' in command or 'meaning' in command:
+        word_meaning(command.split(" ")[-1])
+    
+    if 'send' in command and 'mail' in command:
+        send_mail()
     pass
 
 
--- utils.py
+++ utils.py
@@ -2,6 +2,14 @@ from time import sleep
 import sys
 import requests
 import os
+from datetime import date
+from datetime import datetime, timedelta
+import json
+import smtplib
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders
 
 
 def clear_screen():
@@ -10,13 +18,23 @@ def clear_screen():
 
 def interactive_print(input_string):
     for char in input_string:
-        sleep(0)
+        sleep(0.1)
         sys.stdout.write(char)
         sys.stdout.flush()
     print()
 
 
 def get_date():
+    today = date.today()
+    interactive_print('Alexa: Today\'s Date - '+str(today))
+    pass
+
+def get_privious_date():
+    interactive_print((datetime.now() - timedelta(1)).strftime('%Y-%m-%d'))
+    pass
+
+def get_next_date():
+    interactive_print((datetime.now() + timedelta(1)).strftime('%Y-%m-%d'))
     pass
 
 
@@ -35,8 +53,45 @@ def delete_list():
 
 def tell_joke():
     res = requests.get("https://geek-jokes.sameerkumar.website/api")
-    print(res.text)
+    interactive_print(res.text)
     pass
 
 def ask_riddle():
+    pass
+
+def who_is():
+    pass
+
+def word_meaning(word):
+    url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define"
+    querystring = {"term":word}
+    headers = {
+    'x-rapidapi-host': "mashape-community-urban-dictionary.p.rapidapi.com",
+    'x-rapidapi-key': "03487d8506msh9846417c3bc421cp1cf83cjsndb11b228b3d8"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    final_dictionary = json.loads(response.text)
+    meaning = str(final_dictionary['list'][0]['definition']).replace("[","").replace("]","")
+    interactive_print('Alexa: Meaning of word '+str(word)+' is '+meaning)
+    pass
+
+def send_mail():
+    interactive_print('Alexa: What is the receiver\'s email address ?')
+    email_id_str = input('You: ')
+    for data in email_id_str.split(" "):
+        if '@' in data:
+            email_id = data
+    interactive_print('Alexa: What will be the subject of email ?')
+    subject = input('You: ')
+    interactive_print('Alexa: What will be the body of email ?')
+    body = input('You: ')
+    sender_id = os.environ['gmail_address']
+    sender_password = os.environ['gmail_password']
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.ehlo()
+    server.starttls()
+    server.login(sender_id, sender_password)
+    message = 'Subject: {}\n\n{}'.format(subject, body)
+    server.sendmail(sender_id, email_id, message)
+    server.quit()
     pass
